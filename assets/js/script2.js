b8ebaab43ef53c8f021a7332d2b0496136a74220
Paichien <b8350580@gmail.com>
2023-05-02 00:24:56 +0800

    print out userChoice colour

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -4,7 +4,8 @@ const imgContainer = document.querySelector('.imgContainer');
 const creditLineEl = document.querySelector('.creditLine');
 const yourMixEl = document.querySelector('.yourMix');
 const targetColourEl = document.querySelector('.target');
-const userChoiceEl = document.querySelector('.userChoice');
+const userChoiceEl = document.querySelectorAll('.userChoice');
+const userChoiceContainer = document.querySelector('.userChoiceContainer')
 const startBtn = document.querySelector('.startBtn');
 const section1El = document.querySelector('.section1');
 const section2El = document.querySelector('.section2');
@@ -13,7 +14,7 @@ const randomPage = Math.floor(Math.random() * 100) + 1;
 const pexelsURL = `https://api.pexels.com/v1/search?query=landscape&orientation=landscape&per_page=1&page=${randomPage}`;
 
 
-function playGame(){
+function generatePhoto(){
     startBtn.style.display='none';
     section1El.style.display='block';
     section2El.style.display='block';
@@ -32,83 +33,106 @@ function playGame(){
       imageEL.setAttribute('src', photoUrl);
       imgContainer.append(imageEL);
 
-      const googleVisionAPIUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKeyGoogle}`;
-      const requestData = {
-        "requests": [
-          {
-            "image": {
-              "source": {
-                "imageUri": photoUrl
-              }
-            },
-            "features": [
-              {
-                "type": "IMAGE_PROPERTIES"
-              }
-            ]
-          }
-        ]
-      };
-      fetch(googleVisionAPIUrl, {
-        method: 'POST',
-        body: JSON.stringify(requestData),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        const colors = data.responses[0].imagePropertiesAnnotation.dominantColors.colors;
-        const rgb = colors[0].color;
-        const r = rgb.red;
-        const g = rgb.green;
-        const b = rgb.blue;
-        const dominantRGB= `rgb(${r},${g},${b})`
-
-        targetColourEl.style.backgroundColor = dominantRGB;
-
-        let randomColors = [
-            getRandomColor(),
-            getRandomColor(),
-            getRandomColor(),
-            getRandomColor(),
-          ];
-        
-          
-          for (let i = 0; i < userChoiceEl.length; i++) {
-          userChoiceEl[i].style.backgroundColor = randomColors[i];
-         }
-        
-         
-         const index1= Math.floor(Math.random()*4);
-         let index2 = math.floor(Math.random()*4);
-        
-         while (index1===index2) {
-            index2 = math.floor(Math.random()*4);    
-         }
-        
-         const r2 = r*2;
-         const g2 = g*2;
-         const b2 = b*2;
-
-         const randomColorsR = r2-getRandomInt(r2.length);
-         const randomColorsG = g2-getRandomInt(g2.length);
-         const randomColorsB = b2-getRandomInt(b2.length);
-
-         randomColors[index1] = 'rgb('+randomColorsR+','+randomColorsG+','+randomColorsB+')';
-         randomColors[index2] = 'rgb('+ r2-randomColorsR+g2-randomColorsG+b2-randomColorsB+')';
-         
-
-    })
+      createDominantColor(photoUrl);})
     .catch(error => console.error(error));
-})
-.catch(error => console.error(error));
+}
 
 
+function createDominantColor(photoUrl){
+    const googleVisionAPIUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKeyGoogle}`;
+    const requestData = {
+      "requests": [
+        {
+          "image": {
+            "source": {
+              "imageUri": photoUrl
+            }
+          },
+          "features": [
+            {
+              "type": "IMAGE_PROPERTIES"
+            }
+          ]
+        }
+      ]
+    };
+    fetch(googleVisionAPIUrl, {
+      method: 'POST',
+      body: JSON.stringify(requestData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const colors = data.responses[0].imagePropertiesAnnotation.dominantColors.colors;
+      const rgb = colors[0].color;
+      const r = rgb.red;
+      const g = rgb.green;
+      const b = rgb.blue;
+      const dominantRGB= `rgb(${r},${g},${b})`
+    
+      targetColourEl.style.backgroundColor = dominantRGB;
+
+      generateRandomChoice(r,g,b);
+}).catch(error => console.error(error));
+};
 
 
+function generateRandomChoice(r,g,b){
+
+    let randomColors = [
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+      getRandomColor(),
+    ];
+  
+   const index1= Math.floor(Math.random()*4);
+   let index2 = Math.floor(Math.random()*4);
+  
+   while (index1===index2) {
+      index2 = Math.floor(Math.random()*4);    
+   }
+  
+   const r2 = r*2;
+   const g2 = g*2;
+   const b2 = b*2;
+
+   const randomColorsR1 = r2-getRandomInt(r2);
+   const randomColorsG1 = g2-getRandomInt(g2);
+   const randomColorsB1 = b2-getRandomInt(b2);
+
+   const randomColorsR2 = r2-randomColorsR1;
+   const randomColorsG2 = g2-randomColorsG1;
+   const randomColorsB2 = b2-randomColorsB1;
+
+   randomColors[index1] = `rgb(${randomColorsR1},${randomColorsG1},${randomColorsB1})`;
+   randomColors[index2] = `rgb(${randomColorsR2},${randomColorsG2},${randomColorsB2})`;
+
+   for (let i = 0; i < 4; i++) {
+    userChoiceEl[i].style.backgroundColor = randomColors[i];
+   }
+
+   userChoiceContainer.addEventListener('click', renderUserChoice(e, ))
+
+   console.log(randomColors[index1], randomColors[index2], r,g,b);
 
 }
+
+function renderUserChoice(e){
+    if (e.target!==userChoiceEl){
+        return
+    }else if (e.target===)
+
+}
+
+function getRandomColor(){
+
+    const color = `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
+    return color;
+}
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
@@ -118,4 +142,4 @@ function getRandomInt(max) {
 
 
 
-startBtn.addEventListener('click', playGame)
+startBtn.addEventListener('click', generatePhoto)
